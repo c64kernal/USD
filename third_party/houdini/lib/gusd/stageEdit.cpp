08ccbcc9a11d20d2574b2e078723a32f546f1e9f
George ElKoura <c64kernal@gmail.com>
2018-09-10 17:20:19 -0700

    Remove GusdStageEdit abstraction in favor of a simpler implementation.

--- third_party/houdini/lib/gusd/stageEdit.cpp
+++ third_party/houdini/lib/gusd/stageEdit.cpp
@@ -32,7 +32,7 @@ PXR_NAMESPACE_OPEN_SCOPE
 
 
 bool
-GusdStageBasicEdit::Apply(const SdfLayerHandle& layer,
+GusdStageEdit::Apply(const SdfLayerHandle& layer,
                           UT_ErrorSeverity sev) const
 {
     SdfChangeBlock changeBlock;
@@ -45,7 +45,7 @@ GusdStageBasicEdit::Apply(const SdfLayerHandle& layer,
 
 
 bool
-GusdStageBasicEdit::Apply(const UsdStagePtr& stage,
+GusdStageEdit::Apply(const UsdStagePtr& stage,
                           UT_ErrorSeverity sev) const
 {
     UT_ASSERT_P(stage);
@@ -57,7 +57,7 @@ GusdStageBasicEdit::Apply(const UsdStagePtr& stage,
 
 
 size_t
-GusdStageBasicEdit::GetHash() const
+GusdStageEdit::GetHash() const
 {
     size_t hash = SYShashRange(_variants.begin(), _variants.end());
     SYShashCombine(hash, SYShashRange(_layersToMute.begin(),
@@ -67,9 +67,9 @@ GusdStageBasicEdit::GetHash() const
 
 
 bool
-GusdStageBasicEdit::operator==(const GusdStageEdit& o) const
+GusdStageEdit::operator==(const GusdStageEdit& o) const
 {
-    if(const auto* oPtr = dynamic_cast<const GusdStageBasicEdit*>(&o)) {
+    if(const auto* oPtr = dynamic_cast<const GusdStageEdit*>(&o)) {
         return _variants == oPtr->_variants &&
                _layersToMute == oPtr->_layersToMute;
     }
@@ -78,17 +78,17 @@ GusdStageBasicEdit::operator==(const GusdStageEdit& o) const
 
 
 void
-GusdStageBasicEdit::GetPrimPathAndEditFromVariantsPath(
+GusdStageEdit::GetPrimPathAndEditFromVariantsPath(
     const SdfPath& pathWithVariants,
     SdfPath& primPath,
-    GusdStageBasicEditPtr& edit)
+    GusdStageEditPtr& edit)
 {
     SdfPath variants;
     GusdUSD_Utils::ExtractPrimPathAndVariants(pathWithVariants,
                                               primPath, variants);
     if(!variants.IsEmpty()) {
         if(!edit)
-            edit.reset(new GusdStageBasicEdit);
+            edit.reset(new GusdStageEdit);
         edit->GetVariants().append(variants);
     }
 }
